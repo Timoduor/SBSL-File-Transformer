774b9414875413c0b8f24a472a663122bc8b198e
Bryson Yida <brysonyida@gmail.com>
2021-07-25 21:54:21 +0300

    disconnect for upload error

--- SbslFileTransformer/Infrastructure/Sftp/SftpManager.cs
+++ SbslFileTransformer/Infrastructure/Sftp/SftpManager.cs
@@ -75,6 +75,8 @@ namespace SbslFileTransformer.Infrastructure.Sftp
                 }
                 catch (Exception exception)
                 {
+                    client.Disconnect();
+
                     _logger.LogError(exception, $"Failed in uploading file [{localFilePath}] to [{remoteFilePath}]");
                 }
             }
