3714617f7dbd08417ad5b9bbd99049b1ea75b1db
Bonsaviour Omuholo <wanga@sbsl.co.ke>
2021-08-11 03:31:58 +0300

    KE EOD Deals

---
+++ SbslFileTransformer/Converters/Kenya/EODDealsKEConverter.cs
@@ -0,0 +1,164 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using ExcelDataReader;
+using System.Data;
+
+
+namespace SbslFileTransformer.Converters.Kenya
+{
+    public class EODDealsKEConverter
+    {
+        public EODDealsKEConverter()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public static void WriteFile(string path, string content)
+        {
+            using (var fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (var sw = new StreamWriter(fs))
+                    sw.Write(content);
+            }
+        }
+        public void POSTEOD_ConvertFile(string inputFile)
+        {
+     
+            var list2 = new List<ExcelCols>();
+            var list3 = new List<ExcelCols>();
+            var list4 = new List<ExcelCols>();
+            var list5 = new List<ExcelCols>();
+            var list6 = new List<ExcelCols>();
+            var list7 = new List<ExcelCols>();
+            var list8 = new List<ExcelCols>();
+            var list9 = new List<ExcelCols>();
+            var list10 = new List<ExcelCols>();
+            var list11 = new List<ExcelCols>();
+            var list12 = new List<ExcelCols>();
+            var list13 = new List<ExcelCols>();
+
+            string outputFolder = null;
+            if (string.IsNullOrEmpty(outputFolder))
+            {
+                outputFolder = Path.GetDirectoryName(inputFile);
+            }
+            outputFolder = outputFolder + "\\Conv";
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+
+            string firstdate = "";
+            string lastdate = "";
+
+            string scontent = "";
+            string scontentl2 = "";
+         
+         
+
+            using (var stream = File.Open(inputFile, FileMode.Open, FileAccess.Read))
+            { 
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    var tables = result.Tables;
+
+                    var sheet1 = tables[0];
+
+
+                    foreach (DataRow row in sheet1.Rows)
+                    {
+                        var excelCol = new ExcelCols();
+                        excelCol.Col0 = row[0].ToString();
+                        excelCol.Col1 = row[1].ToString();
+                        excelCol.Col2 = row[2].ToString();
+                        excelCol.Col3 = row[3].ToString();
+                        excelCol.Col4 = row[4].ToString();
+                        excelCol.Col5 = row[5].ToString();
+                        excelCol.Col6 = row[6].ToString();
+                        excelCol.Col7 = row[7].ToString();
+                        excelCol.Col8 = row[8].ToString();
+                        excelCol.Col9 = row[9].ToString();
+                        excelCol.Col10 = row[10].ToString();
+                        excelCol.Col11 = row[11].ToString();
+                        excelCol.Col12 = row[12].ToString();
+                        excelCol.Col13 = row[13].ToString();
+                        excelCol.Col14 = row[14].ToString();
+                        excelCol.Col15 = row[15].ToString();
+                        excelCol.Col16 = row[16].ToString();
+                        excelCol.Col17 = row[17].ToString();
+
+
+
+                        list2.Add(excelCol);
+
+                    }
+
+                }
+            }
+            //Sheet 1
+            var l = list2;
+
+            for (var i = 1; i < list2.Count - 1; i++)
+            {
+                if (list2[i].Col17.Trim() != "Capture Timestamp")
+                {
+                    if (firstdate == "")
+                    {
+
+                        firstdate = Convert.ToDateTime(list2[i].Col17.Trim()).Date.ToString("M/dd/yyyy");
+                    }
+                    else
+                    {
+                        if (firstdate == Convert.ToDateTime(list2[i].Col17.Trim()).Date.ToString("M/dd/yyyy"))
+                        {
+
+                        }
+                        else
+                        {
+                            if (Convert.ToDateTime(firstdate) > Convert.ToDateTime(list2[i].Col17.Trim()).Date)
+                            {
+                                lastdate = firstdate;
+
+
+                            }
+                            else
+                            { lastdate = Convert.ToDateTime(list2[i].Col17.Trim()).Date.ToString("M/dd/yyyy"); }
+
+                        }
+
+                    }
+
+                }
+
+
+
+            }
+
+            for (var i = 0; i < list2.Count - 1; i++)
+            {
+                if (scontentl2 == "")
+                {
+                   
+                    scontentl2 += list2[i].Col0.Trim() + "," + list2[i].Col1 + "," + list2[i].Col2 + "," + list2[i].Col13 + "," + list2[i].Col14 + "," + list2[i].Col5 + "," + list2[i].Col6 + "," + list2[i].Col7 + "," + list2[i].Col8 + "," + list2[i].Col9 + "," + list2[i].Col10 + "," + list2[i].Col11 + "," + list2[i].Col12 + "," + list2[i].Col13 + "," + list2[i].Col14 + "," + list2[i].Col15 + "," + list2[i].Col16 + "," + list2[i].Col17 + Environment.NewLine;
+                            
+
+                }
+                else
+                {
+                    if (Convert.ToDateTime(list2[i].Col17.Trim()).Date.ToString("M/dd/yyyy") == lastdate)
+                    {
+                        scontentl2 += list2[i].Col0.Trim() + "," + list2[i].Col1 + "," + list2[i].Col2 + "," + list2[i].Col13 + "," + list2[i].Col14 + "," + list2[i].Col5 + "," + list2[i].Col6 + "," + list2[i].Col7 + "," + list2[i].Col8 + "," + list2[i].Col9 + "," + list2[i].Col10 + "," + list2[i].Col11 + "," + list2[i].Col12 + "," + list2[i].Col13 + "," + list2[i].Col14 + "," + list2[i].Col15 + "," + list2[i].Col16 + "," + list2[i].Col17 + Environment.NewLine;
+                    }
+                }
+            }
+
+            scontent = scontentl2;
+
+            WriteFile(outputFolder + "\\Converted_EOD_DEAL_" + Path.GetFileNameWithoutExtension(inputFile) + ".csv", scontent);
+        }
+
+    }
+}
---
+++ SbslFileTransformer/Infrastructure/Jobs/Converters/Kenya/EOD_DealsJob.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SbslFileTransformer.Converters.Kenya;
+using SbslFileTransformer.Data;
+using SbslFileTransformer.Infrastructure.Helpers;
+using SbslFileTransformer.Infrastructure.Messaging;
+using SbslFileTransformer.Models.Enums;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SbslFileTransformer.Infrastructure.Jobs.Converters.Kenya
+{
+    public class EOD_DealsJob : ConverterJobBase<EOD_DealsJob>, IHostedService
+    {
+        public EOD_DealsJob(ILogger<EOD_DealsJob> logger, IServiceScopeFactory serviceScopeFactory,
+           EmailSender emailSender)
+        {
+            _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
+            _emailSender = emailSender;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting EOD Deals Converter Job");
+
+            _semaphore = new SemaphoreSlim(1, 1);
+
+            _timer = new Timer(async state => await EOD_DealsConverter(), null,
+                TimeSpan.FromSeconds(new Random().Next(30, 60)), TimeSpan.FromMinutes(10));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _semaphore.Dispose();
+            _timer.Dispose();
+            return Task.CompletedTask;
+        }
+        private async Task EOD_DealsConverter()
+        {
+            try
+            {
+                await _semaphore.WaitAsync();
+
+                _logger.LogInformation("Running EOD Deals Converter job");
+
+                var prodFolder = string.Empty;
+                var sbFolder = string.Empty;
+                var Entity = string.Empty;
+
+                using (var scope = _serviceScopeFactory.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                    var configurations = dbContext.Configurations.Where(c => c.ConfigType == ConfigurationType.Sftp)
+                        .ToList();
+
+                    Entity = dbContext.Configurations
+                        .FirstOrDefault(c => c.ConfigType == ConfigurationType.Setting && c.Key == "Entity").Value;
+                    prodFolder = configurations.FirstOrDefault(c => c.Key == "ProductionFolder")?.Value;
+                    sbFolder = configurations.FirstOrDefault(c => c.Key == "SandboxFolder")?.Value;
+
+                    var options = new EnumerationOptions
+                    { RecurseSubdirectories = true, MatchCasing = MatchCasing.CaseInsensitive };
+
+                    var files = Directory.GetFiles(prodFolder, "*.*", options).Where(f => f.ToLower().EndsWith(".xlsx")).ToList();
+                    files.AddRange(Directory.GetFiles(sbFolder, "*.*", options).Where(f => f.ToLower().EndsWith(".xlsx")));
+                    var EODDealConverter = new EODDealsKEConverter();                    
+
+                    foreach (var file in files)
+                        //SPECIFY FOLDER and file extension above PENDING
+
+                        if ( file.ToLower().Contains("gl_entries") && file.ToLower().Contains("imke") && !file.Contains("Conv"))
+                        {
+                            var fileToProcess =
+                                await dbContext.UploadedFiles.FirstOrDefaultAsync(f =>
+                                    f.FilePath.ToLower() == file.ToLower());
+
+                            if (fileToProcess != null && fileToProcess.Converted == false)
+                                try
+                                {
+                                    EODDealConverter.POSTEOD_ConvertFile(file);
+
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    fileToProcess.Failed = true;
+
+                                    _logger.LogError(ex, ex.Message);
+
+                                    await EmailHelpers.SendEmails(dbContext, "Problem Converting  EOD Deals files",
+                                        $"{file} \n\n {ex.Message}", new[] { file }, _emailSender);
+                                }
+                                finally
+                                {
+                                    fileToProcess.Converted = true;
+
+                                    fileToProcess.ConvertedBy = nameof(EODDealsKEConverter);
+
+                                    dbContext.Update(fileToProcess);
+
+                                    await dbContext.SaveChangesAsync();
+                                }
+                        }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+    }
+}
