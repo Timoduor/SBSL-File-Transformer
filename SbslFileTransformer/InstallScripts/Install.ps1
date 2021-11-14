95da0fca0696b70c17a74f8c6f95f402541c9cbf
Bryson Yida <brysonyida@gmail.com>
2021-11-14 14:07:36 +0300

    Added service installer and uninstaller

---
+++ SbslFileTransformer/InstallScripts/Install.ps1
@@ -0,0 +1,28 @@
+﻿$DisplayName = "SBSL ETL Service"
+
+$ServiceName = "SBSLFileTransformer.exe"
+
+$ServicePath = "..\$ServiceName"
+
+$currentLocation = Get-Location
+
+$executablePath = [IO.Path]::Combine($currentLocation, $ServicePath)
+
+Write-Host "Installing $executablePath ..." -ForegroundColor Yellow
+
+#https://stackoverflow.com/questions/37651152/cannot-start-service-on-computer
+New-Service -Name $ServiceName -BinaryPathName $executablePath -Description "File Extraction Transformation and Loading tool" -StartupType Automatic -DisplayName $DisplayName -Verbose 
+
+Start-Service -name $ServiceName
+
+$action1 = "restart"
+$action2 = "restart"
+$actionLast = ""
+
+$time1 = 5000
+$time2 = 5000
+$timeLast = 1000
+
+$action = $action1+"/"+$time1+"/"+$action2+"/"+$time2+"/"+$actionLast+"//"+$timeLast
+
+sc.exe failure $ServiceName reset= 86400 actions= $action
---
+++ SbslFileTransformer/InstallScripts/Uninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ServiceName = "SBSLFileTransformer.exe"
+
+#note you might need to end the task using Task Manager first due to multi-threading of the application
+Stop-Service -Name $ServiceName
+
+Remove-Service -Name $ServiceName
+
+sc.exe delete $ServiceName
