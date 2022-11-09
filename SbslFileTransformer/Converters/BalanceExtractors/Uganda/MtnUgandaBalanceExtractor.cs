ac154b9ca31d8df00b0ae22d9043446e74d7cd3f
Bonsaviour Omuholo <wanga@sbsl.co.ke>
2022-11-09 12:07:07 +0300

    MTN Bank to wallet balance extractor

--- SbslFileTransformer/Converters/BalanceExtractors/Uganda/MtnUgandaBalanceExtractor.cs
+++ SbslFileTransformer/Converters/BalanceExtractors/Uganda/MtnUgandaBalanceExtractor.cs
@@ -69,6 +69,60 @@ namespace SbslFileTransformer.Converters.BalanceExtractors.Uganda
             }
         }
 
+        public void ConvertFile_b2w(string inputFile, string outputFolder)
+        {
+            List<MtnCols> list = new List<MtnCols>();
+            using (FileStream stream = File.Open(inputFile, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader reader;
+                if (Path.GetExtension(inputFile).ToLower().Contains("csv"))
+                    reader = ExcelReaderFactory.CreateCsvReader(stream);
+                else
+                    reader = ExcelReaderFactory.CreateReader(stream);
+                using (reader)
+                {
+                    // Choose one of either 1 or 2:
+                    // 1. Use the reader methods
+                    while (reader.Read())
+                    {
+                        if (reader.GetValue(0)?.ToString().ToLower().Contains("DATE") ?? false)
+                            continue;
+
+                        MtnCols row = new MtnCols();
+
+                        if (DateTime.TryParseExact(reader.GetValue(0)?.ToString(), "yyyy-MM-dd HH:mm:ss",
+                                CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime resultDate))
+                            row.ReconDate = resultDate;
+                        else
+                            continue;
+
+                        row.Account = "115001204";
+                        row.Amount = Convert.ToDouble(reader.GetValue(22)?.ToString());
+                        list.Add(row);
+                    }
+                }
+            }
+
+            if (list.Count > 0)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(inputFile);
+
+                string fileNameToAppend = fileName.Substring(Math.Max(0, fileName.Length - 13)).Replace(" ", "");
+
+                string outputFile = Path.Combine(outputFolder,
+                    $"MultiCurr_{DateTime.Now:dd_MM_yyyy}_{fileNameToAppend}_MtnUG.txt");
+
+                MtnCols firstRow = list.OrderByDescending(i => i.ReconDate)
+                    .FirstOrDefault(c => c.ReconDate == list.Max(r => r.ReconDate));
+
+                string toAppend =
+                    $"IMUG\t{firstRow.Account}\tMobile banking\t\t\t\t\t\t\t\t\tBalance_bank\t{ContentHelpers.GetLastDayOfTheMonth(firstRow.ReconDate):MM/dd/yyyy}\t\t\t\t{firstRow.Amount}\tUGX\n";
+
+                if (!string.IsNullOrEmpty(toAppend))
+                    File.WriteAllText(outputFile, toAppend);
+            }
+        }
+
         public class MtnCols
         {
             public DateTime ReconDate { get; set; }
