516ec4a7e6210d03a41bcafcda77115ad22c82d0
Bryson Yida <brysonyida@gmail.com>
2023-06-04 18:11:39 +0300

    minor fixes

--- SbslFileTransformer/Converters/Kenya/VisionRecordMatcher.cs
+++ SbslFileTransformer/Converters/Kenya/VisionRecordMatcher.cs
@@ -158,32 +158,39 @@ namespace SbslFileTransformer.Converters.Kenya
                 {
                     while (reader.Read())
                     {
-                        FinacleRec finacleRec = new FinacleRec();
-
-                        finacleRec.AccountNumber = reader.GetString(0);
-                        finacleRec.Currency = reader.GetString(1);
-                        finacleRec.ReferenceNumber = reader.GetString(2);
-                        finacleRec.CardNumber = reader.GetString(3);
-                        finacleRec.TransDate = reader.GetString(4);
-                        finacleRec.ValueDate = Convert.ToDateTime(reader.GetString(5));
-                        finacleRec.TransactionTime = reader.GetString(6);
-                        finacleRec.Ref1 = reader.GetString(7);
-                        finacleRec.Ref2 = reader.GetString(8);
-                        finacleRec.Ref3 = reader.GetString(9);
-                        finacleRec.Ref4 = reader.GetString(10);
-                        finacleRec.DebitCredit = reader.GetString(11);
-                        finacleRec.Amount = Convert.ToDouble(reader.GetString(12));
-                        finacleRec.TransactionParticular = reader.GetString(13);
-                        finacleRec.TransactionID = reader.GetString(14);
-                        if (DateTime.TryParseExact(reader.GetString(15), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime transDate))
+                        try
                         {
-                            finacleRec.TransactionDate = transDate;
-                        }
-                        finacleRec.Time = reader.GetString(16);
-                        finacleRec.Ref5 = reader.GetString(17);
-                        finacleRec.Branch = reader.GetString(18);
+                            FinacleRec finacleRec = new FinacleRec();
 
-                        finacleRecs.Add(finacleRec);
+                            finacleRec.AccountNumber = reader.GetString(0);
+                            finacleRec.Currency = reader.GetString(1);
+                            finacleRec.ReferenceNumber = reader.GetString(2);
+                            finacleRec.CardNumber = reader.GetString(3);
+                            finacleRec.TransDate = reader.GetString(4);
+                            finacleRec.ValueDate = Convert.ToDateTime(reader.GetString(5));
+                            finacleRec.TransactionTime = reader.GetString(6);
+                            finacleRec.Ref1 = reader.GetString(7);
+                            finacleRec.Ref2 = reader.GetString(8);
+                            finacleRec.Ref3 = reader.GetString(9);
+                            finacleRec.Ref4 = reader.GetString(10);
+                            finacleRec.DebitCredit = reader.GetString(11);
+                            finacleRec.Amount = Convert.ToDouble(reader.GetString(12));
+                            finacleRec.TransactionParticular = reader.GetString(13);
+                            finacleRec.TransactionID = reader.GetString(14);
+                            if (DateTime.TryParseExact(reader.GetString(15), "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime transDate))
+                            {
+                                finacleRec.TransactionDate = transDate;
+                            }
+                            finacleRec.Time = reader.GetString(16);
+                            finacleRec.Ref5 = reader.GetString(17);
+                            finacleRec.Branch = reader.GetString(18);
+
+                            finacleRecs.Add(finacleRec);
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, $"Error reading Finacle file {cmsFile}");
+                        }
                     }
                 }
                 stream.Close();
