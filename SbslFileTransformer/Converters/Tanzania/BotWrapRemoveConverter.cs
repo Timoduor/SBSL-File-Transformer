ff8e87d7795cb5833619a012303d3e95d7c34d9a
Bryson <brysonyida@hotmail.com>
2024-03-19 21:49:18 +0300

    added BOT wrap remover

---
+++ SbslFileTransformer/Converters/Tanzania/BotWrapRemoveConverter.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+using CsvHelper;
+
+using ExcelDataReader;
+
+using SbslFileTransformer.Converters.Rwanda.BNR;
+
+namespace SbslFileTransformer.Converters.Tanzania
+{
+    public class BotWrapRemoveConverter
+    {
+        public BotWrapRemoveConverter()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public void ConvertFile(string inputFile, string outputFile = null)
+        {
+            List<ExcelCols> list = new List<ExcelCols>();
+
+            using (FileStream stream = File.Open(inputFile, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader reader;
+
+                if (Path.GetExtension(inputFile).ToLower().EndsWith(".csv"))
+                    reader = ExcelReaderFactory.CreateCsvReader(stream);
+                else
+                    reader = ExcelReaderFactory.CreateReader(stream);
+
+                using (reader)
+                {
+                    // Choose one of either 1 or 2:
+                    // 1. Use the reader methods
+
+                    while (reader.Read())
+                    {
+                        ExcelCols row = new ExcelCols();
+
+                        row.Col0 = reader.GetValue(0)?.ToString();
+
+                        row.Col1 = reader.GetValue(1)?.ToString();
+
+                        row.Col2 = reader.GetValue(2)?.ToString();
+
+                        row.Col3 = reader.GetValue(3)?.ToString();
+
+                        row.Col4 = reader.GetValue(4)?.ToString();
+
+                        row.Col5 = reader.GetValue(5)?.ToString().Replace('\n', ' ').Replace('\r', ' ');
+
+                        row.Col6 = reader.GetValue(6)?.ToString();
+
+                        row.Col7 = reader.GetValue(7)?.ToString();
+
+                        row.Col8 = reader.GetValue(8)?.ToString();
+
+                        row.Col9 = reader.GetValue(9)?.ToString();
+
+                        row.Col10 = reader.GetValue(10)?.ToString();
+
+                        row.Col11 = reader.GetValue(11)?.ToString();
+
+                        row.Col12 = reader.GetValue(12)?.ToString();
+
+                        row.Col13 = reader.GetValue(13)?.ToString();
+
+                        list.Add(row);
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(outputFile))
+            {
+                string outputFolder = Path.Combine(Path.GetDirectoryName(inputFile), "Conv");
+
+                Directory.CreateDirectory(outputFolder);
+
+                string fileName = Path.GetFileNameWithoutExtension(inputFile);
+
+                outputFile = Path.Combine(outputFolder,
+                    $"{DateTime.Now:yyyy_MM_dd_HH_mm_ss}_BOTwrap_{fileName.Substring(Math.Max(0, fileName.Length - 14)).Replace(" ", "")}.csv");
+            }
+
+            this.WriteToFile(list, outputFile);
+        }
+
+        private void WriteToFile(List<ExcelCols> rows, string outputFile)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    foreach (ExcelCols row in rows)
+                    {
+                        csv.WriteRecord(row);
+                        csv.NextRecord();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SbslFileTransformer/Infrastructure/Jobs/Converters/Tanzania/BotWrapRemoverJob.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using SbslFileTransformer.Converters.Tanzania;
+using SbslFileTransformer.Data;
+using SbslFileTransformer.Infrastructure.Messaging;
+using SbslFileTransformer.Models;
+using SbslFileTransformer.Models.Enums;
+
+namespace SbslFileTransformer.Infrastructure.Jobs.Converters.Tanzania
+{
+    public class BotWrapRemoverJob : ConverterJobBase<BotWrapRemoverJob>, IHostedService
+    {
+        protected override string JobName { get; set; } = nameof(BotWrapRemoverJob);
+
+        public BotWrapRemoverJob(ILogger<BotWrapRemoverJob> logger,
+            IServiceScopeFactory serviceScopeFactory, EmailSender emailSender)
+        {
+            this._logger = logger;
+            this._serviceScopeFactory = serviceScopeFactory;
+            this._emailSender = emailSender;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this._logger.LogInformation("Starting BOT Wrap Converter Job");
+
+            _semaphore = new SemaphoreSlim(1, 1);
+
+            this._timer = new Timer(async state => await this.ConvertBotWrapFile(), null,
+                TimeSpan.FromSeconds(new Random().Next(60, 200)), TimeSpan.FromMinutes(10));
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ConvertBotWrapFile()
+        {
+            try
+            {
+                await _semaphore.WaitAsync();
+
+                this._logger.LogInformation("Running BOT Wrap file converter job");
+
+                string prodFolder = string.Empty;
+                string sbFolder = string.Empty;
+                string Entity = string.Empty;
+
+                using (IServiceScope scope = this._serviceScopeFactory.CreateScope())
+                {
+                    ApplicationDbContext dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                    List<Configuration> configurations = dbContext.Configurations.ToList();
+
+                    Entity = configurations
+                        .FirstOrDefault(c => c.ConfigType == ConfigurationType.Setting && c.Key == "Entity").Value;
+                    prodFolder = configurations.FirstOrDefault(c => c.Key == "ProductionFolder")?.Value;
+                    sbFolder = configurations.FirstOrDefault(c => c.Key == "SandboxFolder")?.Value;
+
+
+                    EnumerationOptions options = new EnumerationOptions
+                    { RecurseSubdirectories = true, MatchCasing = MatchCasing.CaseInsensitive };
+
+                    List<string> files = Directory.GetFiles(prodFolder, "*.*", options).Where(f => f.ToLower().EndsWith(".csv"))
+                        .ToList();
+
+                    files.AddRange(
+                        Directory.GetFiles(sbFolder, "*.*", options).Where(f => f.ToLower().EndsWith(".csv")));
+
+                    var mpesaConverter = new BotWrapRemoveConverter();
+
+                    List<SftpUploadedFile> uploadedFiles = await dbContext.UploadedFiles.ToListAsync();
+
+                    List<SftpUploadedFile> updatedFiles = new List<SftpUploadedFile>();
+
+
+                    foreach (string file in files)
+                    {
+                        //PATH IS STILL PENDING
+
+                        if (file.ToLower().Contains("imtz") && file.ToLower().Contains("nostro")
+                            && file.ToLower().Contains("bot") && !file.Contains("Conv"))
+                        {
+                            SftpUploadedFile fileToProcess =
+                                uploadedFiles.FirstOrDefault(f =>
+                                    f.FilePath.ToLower() == file.ToLower());
+
+                            if (fileToProcess != null && fileToProcess.Converted == false)
+                                try
+                                {
+                                    mpesaConverter.ConvertFile(file);
+                                }
+                                catch (Exception ex)
+                                {
+                                    await this.ProcessFileFailure(configurations, file, fileToProcess, ex);
+                                }
+                                finally
+                                {
+                                    this.CompleteFileProcessing(updatedFiles, fileToProcess, nameof(SuspenseTachFileConverter));
+                                }
+                        }
+                    }
+                    await this.SaveProcessedFilesStatuses(dbContext, updatedFiles);
+                }
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, ex.Message);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
