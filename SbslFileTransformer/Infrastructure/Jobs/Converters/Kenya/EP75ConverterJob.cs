965cdf78fa1d8b04fe15c9f5db6f494fc718954d
Bryson Yida <brysonyida@hotmail.com>
2024-12-29 21:39:13 +0300

    add extra file for EP75 conversion

--- SbslFileTransformer/Infrastructure/Jobs/Converters/Kenya/EP75ConverterJob.cs
+++ SbslFileTransformer/Infrastructure/Jobs/Converters/Kenya/EP75ConverterJob.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
 using SbslFileTransformer.Converters.Kenya;
 using SbslFileTransformer.Data;
 using SbslFileTransformer.Infrastructure.Messaging;
@@ -21,19 +23,19 @@ namespace SbslFileTransformer.Infrastructure.Jobs.Converters.Kenya
         public EP75ConverterJob(ILogger<EP75ConverterJob> logger, IServiceScopeFactory serviceScopeFactory,
             EmailSender emailSender)
         {
-            this._logger = logger;
-            this._serviceScopeFactory = serviceScopeFactory;
-            this._emailSender = emailSender;
+            _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
+            _emailSender = emailSender;
         }
 
         protected override string JobName { get; set; } = nameof(EP75ConverterJob);
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            this._logger.LogInformation("Starting EP75 Converter Job");
+            _logger.LogInformation("Starting EP75 Converter Job");
 
             _semaphore = new SemaphoreSlim(1, 1);
 
-            this._timer = new Timer(async state => await this.ConvertEP75File(), null,
+            _timer = new Timer(async state => await ConvertEP75File(), null,
                 TimeSpan.FromSeconds(new Random().Next(60, 200)), TimeSpan.FromMinutes(10));
 
             return Task.CompletedTask;
@@ -45,17 +47,17 @@ namespace SbslFileTransformer.Infrastructure.Jobs.Converters.Kenya
             {
                 await _semaphore.WaitAsync();
 
-                this._logger.LogInformation("Running EP75 converter job");
+                _logger.LogInformation("Running EP75 converter job");
 
-                string prodFolder = string.Empty;
-                string sbFolder = string.Empty;
-                string Entity = string.Empty;
+                var prodFolder = string.Empty;
+                var sbFolder = string.Empty;
+                var Entity = string.Empty;
 
-                using (IServiceScope scope = this._serviceScopeFactory.CreateScope())
+                using (var scope = _serviceScopeFactory.CreateScope())
                 {
-                    ApplicationDbContext dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                    var dbContext = scope.ServiceProvider.GetService<ApplicationDbContext>();
 
-                    List<Configuration> configurations = await dbContext.Configurations.ToListAsync();
+                    var configurations = await dbContext.Configurations.ToListAsync();
 
                     Entity = configurations
                         .FirstOrDefault(c => c.ConfigType == ConfigurationType.Setting && c.Key == "Entity")?.Value;
@@ -63,52 +65,56 @@ namespace SbslFileTransformer.Infrastructure.Jobs.Converters.Kenya
                     prodFolder = configurations.FirstOrDefault(c => c.Key == "ProductionFolder")?.Value;
                     sbFolder = configurations.FirstOrDefault(c => c.Key == "SandboxFolder")?.Value;
 
-                    EnumerationOptions options = new EnumerationOptions
+                    var options = new EnumerationOptions
                     { RecurseSubdirectories = true, MatchCasing = MatchCasing.CaseInsensitive };
 
-                    List<string> files = Directory.GetFiles(prodFolder, "*.txt", options).ToList();
+                    var files = Directory.GetFiles(prodFolder, "*.txt", options).ToList();
 
                     files.AddRange(Directory.GetFiles(sbFolder, "*.txt", options));
 
-                    EP75Converter epinConverter = new EP75Converter();
+                    var epinConverter = new EP75Converter();
 
-                    List<SftpUploadedFile> uploadedFiles = await dbContext.UploadedFiles.ToListAsync();
+                    var uploadedFiles = await dbContext.UploadedFiles.ToListAsync();
 
-                    List<SftpUploadedFile> updatedFiles = new List<SftpUploadedFile>();
+                    var updatedFiles = new List<SftpUploadedFile>();
 
-                    foreach (string file in files)
+                    foreach (var file in files)
                     {
-                        if (file.ToLower().Contains("ep75") && file.ToLower().Contains("imke") &&
-                            file.ToLower().Contains("cards"))
+                        if ((file.ToLower().Contains("ep75") && file.ToLower().Contains("imke") &&
+                            file.ToLower().Contains("cards")) ||
+                            (file.ToLower().Contains("imug") && file.ToLower().Contains("visa") &&
+                            file.ToLower().Contains("vc_acquired")))
                         {
-                            SftpUploadedFile fileToProcess = uploadedFiles.FirstOrDefault(f =>
+                            var fileToProcess = uploadedFiles.FirstOrDefault(f =>
                                     f.FilePath.ToLower() == file.ToLower());
 
                             if (fileToProcess != null && fileToProcess.Converted == false)
+                            {
                                 try
                                 {
                                     epinConverter.ConvertFile(file);
                                 }
                                 catch (Exception ex)
                                 {
-                                    await this.ProcessFileFailure(configurations, file, fileToProcess, ex);
+                                    await ProcessFileFailure(configurations, file, fileToProcess, ex);
                                 }
                                 finally
                                 {
-                                    this.CompleteFileProcessing(updatedFiles, fileToProcess, nameof(EP75Converter));
+                                    CompleteFileProcessing(updatedFiles, fileToProcess, nameof(EP75Converter));
                                 }
+                            }
                         }
                     }
-                    await this.SaveProcessedFilesStatuses(dbContext, updatedFiles);
+                    await SaveProcessedFilesStatuses(dbContext, updatedFiles);
                 }
             }
             catch (Exception ex)
             {
-                this._logger.LogError(ex, ex.Message);
+                _logger.LogError(ex, ex.Message);
             }
             finally
             {
-                _semaphore.Release();
+                _ = _semaphore.Release();
             }
         }
     }
